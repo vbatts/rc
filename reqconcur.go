9d66e077dd42d5eccb7062d49b58df4ed09f8345
Vincent Batts <vbatts@hashbangbash.com>
2013-10-29 14:29:24 -0400

    closer, but something is hanging the workers

--- reqconcur.go
+++ reqconcur.go
@@ -11,18 +11,18 @@ import (
 	"log"
 	"net/http"
 	"os"
+  "os/signal"
 )
 
 var (
 	config_url, config_cert, config_key  string
-	config_requests, config_workers      int
+	config_requests, config_workers      int64
 	config_head_method, config_fail_quit bool
 	config_quiet                         bool
 	routines                             int = 15
 	results                              chan stat
-	quit_now                             chan *http.Response = make(chan *http.Response)
+	fail_now                             chan *http.Response = make(chan *http.Response)
 	codes                                map[int]uint        = make(map[int]uint)
-	count                                int
 	cert                                 tls.Certificate
 	err                                  error
 )
@@ -31,14 +31,41 @@ func init() {
 	flag.StringVar(&config_url, "url", "", "url to fetch (like https://www.mydomain.com/content/)")
 	flag.StringVar(&config_cert, "cert", "", "x509 certificate file to use")
 	flag.StringVar(&config_key, "key", "", "RSA key file to use")
-	flag.IntVar(&config_requests, "requests", 10, "Number of requests to perform")
-	flag.IntVar(&config_workers, "workers", 5, "Number of workers to use")
+	flag.Int64Var(&config_requests, "requests", 10, "Number of requests to perform")
+	flag.Int64Var(&config_workers, "workers", 5, "Number of workers to use")
 	flag.BoolVar(&config_head_method, "head", false, "Whether to use HTTP HEAD (default is GET)")
 	flag.BoolVar(&config_fail_quit, "fail", false, "Whether to exit on a non-OK response")
 	flag.BoolVar(&config_quiet, "quiet", false, "do not print all responses")
 }
 
+type Requests struct {
+	Method string
+	Url    string
+	Total  int64
+}
+
+func (r *Requests) Next() (*http.Request, error) {
+	defer func() { r.Total = r.Total - 1 }()
+	return http.NewRequest(r.Method, r.Url, nil)
+}
+
 func main() {
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, os.Interrupt)
+		signal.Notify(c, os.Kill)
+		for sig := range c {
+			// sig is a ^C, handle it
+			if sig == os.Interrupt {
+				log.Println("interrupted ...")
+				panic("showing stack")
+			} else if sig == os.Kill {
+				log.Println("killing ...")
+				panic("showing stack")
+			}
+		}
+	}()
+
 	flag.Parse()
 
 	t_config := tls.Config{
@@ -68,38 +95,53 @@ func main() {
 	log.Printf("Please wait, calling against [%s] ...", config_url)
 	results = make(chan stat, config_workers)
 	workers := make(chan *http.Request, config_workers)
-	for i := 0; i < config_workers; i++ {
+
+	// rev up these workers
+	for i := int64(0); i < config_workers; i++ {
 		go func() {
-			for j := 0; j < config_requests; j++ {
-				var (
-					req *http.Request
-					err error
-				)
-				if config_head_method {
-					req, err = http.NewRequest("HEAD", config_url, nil)
-				} else {
-					req, err = http.NewRequest("GET", config_url, nil)
-				}
+			// this goroutine needs a channel to receive and cleanup
+			for {
+				resp, err := client.Do(<-workers)
 				if err != nil {
 					log.Printf("ERROR: setting up request %s", err)
-					return
+					if config_fail_quit {
+						os.Exit(2)
+					}
 				}
-				resp, err := client.Do(req)
-				if err != nil {
-					log.Printf("ERROR: %s", err)
-				} else {
+				go func() {
+					// this goroutine will spin off and get harvested
 					codes[resp.StatusCode]++
-					go func() {
-						results <- respStat(resp)
-					}()
-				}
-				if resp.StatusCode != 200 {
-					quit_now <- resp
-				}
-				count++
+					results <- respStat(resp)
+					if config_fail_quit && resp.StatusCode != 200 {
+						fail_now <- resp
+					}
+				}()
+        resp.Body.Close()
 			}
 		}()
 	}
+
+	reqs := &Requests{
+		Url:   config_url,
+		Total: config_requests,
+	}
+	if config_head_method {
+		reqs.Method = "HEAD"
+	} else {
+		reqs.Method = "GET"
+	}
+
+	for reqs.Total != 0 {
+		r, err := reqs.Next()
+		if err != nil {
+			log.Printf("ERROR: setting up request %s", err)
+			if config_fail_quit {
+				os.Exit(2)
+			}
+		}
+		workers <- r
+	}
+
 	for {
 		select {
 		case r := <-results:
@@ -107,13 +149,10 @@ func main() {
 			if !config_quiet {
 				log.Println(r)
 			}
-		case req := <-quit_now:
-			log.Printf("made %d requests before failure", count)
+		case req := <-fail_now:
+			log.Printf("made %d requests before failure", (reqs.Total - config_requests))
 			log.Printf("ERROR: %#v", req)
 			os.Exit(2)
-		}
-		if count == (config_requests * config_workers) {
-			break
 		}
 	}
 	log.Println("HTTP Codes:")
